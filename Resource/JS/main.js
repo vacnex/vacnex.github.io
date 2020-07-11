338f74e602288fbef2a3e507c7eae4e5d0f8c7a1
VAC <37587399+vacnex@users.noreply.github.com>
2020-07-11 21:49:04 +0700

    First Init

---
+++ Resource/JS/main.js
@@ -0,0 +1,38 @@
+(function($){
+    'use strict';
+    var nav_offset_top = $('header').height() + 50;
+    function navbarFixed() {
+		if ($('.header_area').length) {
+			$(window).scroll(function() {
+				var scroll = $(window).scrollTop();
+				if (scroll >= nav_offset_top) {
+					$('.header_area').addClass('navbar_fixed');
+				} else {
+					$('.header_area').removeClass('navbar_fixed');
+				}
+			});
+		}
+	}
+    navbarFixed();
+    $(window).on('load', function() {
+		$('.portfolio-filter ul li').on('click', function() {
+			$('.portfolio-filter ul li').removeClass('active');
+			$(this).addClass('active');
+
+			var data = $(this).attr('data-filter');
+			$workGrid.isotope({
+				filter: data
+			});
+		});
+
+		if (document.getElementById('portfolio')) {
+			var $workGrid = $('.portfolio-grid').isotope({
+				itemSelector: '.all',
+				percentPosition: true,
+				masonry: {
+					columnWidth: '.all'
+				}
+			});
+		}
+    });
+})(jQuery);

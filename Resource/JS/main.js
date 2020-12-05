bd58ba06301298e05c4e0910fe514c59f1a133b4
VAC <37587399+vacnex@users.noreply.github.com>
2020-12-05 23:39:45 +0700

    Update Profilo
    - Update link to project github
    - Add 2 category
    - Add skill section
    - Add two new project

--- Resource/JS/main.js
+++ Resource/JS/main.js
@@ -79,5 +79,19 @@
 			$('#titem').addClass('active');
 		}
 	});
+	$('.skill-per').each(function(){
+        var $this = $(this);
+        var per = $this.attr('per');
+        $this.css("width",per+'%');
+        $({animatedValue: 0}).animate({animatedValue: per},{
+          duration: 1000,
+          step: function(){
+            $this.attr('per', Math.floor(this.animatedValue) + '%');
+          },
+          complete: function(){
+            $this.attr('per', Math.floor(this.animatedValue) + '%');
+          }
+        });
+      });
 	// console.log();
 })(jQuery);
